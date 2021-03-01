d3bac466cc9b434ab63129a20f39865750b62a74
Lívia Delgado <livia.delgado.ac@gmail.com>
2021-03-01 17:16:12 -0300

    fix: quick sort optimized - change to be just like qs

--- src/sort/quickSortOptimized.cpp
+++ src/sort/quickSortOptimized.cpp
@@ -14,50 +14,34 @@ returnToEarth::Planet* QuickSortOptimized::sort(returnToEarth::Planet* list, int
     return orderedList;
 }
 
-void QuickSortOptimized::quickSort(returnToEarth::Planet* list, int left, int right) {
-    int stack[right - left + 1]; 
-
-	int top = -1; 
-
-	stack[++top] = left; 
-	stack[++top] = right; 
-
-	while (top >= 0) {
-		right = stack[top--]; 
-		left = stack[top--]; 
-
-		int pivot = partition(list, left, right); 
-
-		if (pivot - 1 > left) { 
-			stack[++top] = left; 
-			stack[++top] = pivot - 1; 
-		} 
-
-		if (pivot + 1 < right) { 
-			stack[++top] = pivot + 1; 
-			stack[++top] = right;
-		} 
-	} 
-}
-
 void QuickSortOptimized::swap(returnToEarth::Planet* a, returnToEarth::Planet* b) {
 	returnToEarth::Planet aux = *a;
 	*a = *b;
 	*b = aux;
 } 
 
+void QuickSortOptimized::quickSort(returnToEarth::Planet* list, int left, int right) {
+    int pivot;
+
+    if (left < right) {
+        pivot = partition(list, left, right);
+        quickSort(list, left, pivot-1);
+        quickSort(list, pivot+1, right);
+    }
+}
+
 int QuickSortOptimized::partition(returnToEarth::Planet* list, int left, int right) {
-	returnToEarth::Planet auxiliar = list[right];
-	int i = (left - 1);
+    returnToEarth::Planet pivot = list[(left+right)/2];
+    int i = (left - 1);
+ 
+    for (int j = left; j <= right - 1; j++) {
+        if (list[j].getDistanceFromEarth() >= pivot.getDistanceFromEarth())
+        {
+            i++;
+            swap(&list[i], &list[j]);
+        }
+    }
+    swap(&list[i + 1], &list[right]);
 
-	for (int j = left; j <= right - 1; j++) {
-		if (list[j].getDistanceFromEarth() >= auxiliar.getDistanceFromEarth()) {
-			i++;
-			swap(&list[i], &list[j]);
-		}
-	}
-
-	swap(&list[i + 1], &list[right]);
-
-	return (i+1);
+    return (i + 1);
 }
